13c7f56b1380e28d543a65de39ce2e3f0839aa1c
hvellyr <gck@eystep.org>
2013-11-13 11:16:43 +0100

    Rewrite
    - based on css mode, which gives us some nice indentation out of the box
    - adding more keywords
    - changed some font lock regex
    - a number of basic commands
    - simple abbrev expanding

--- qml-mode.el
+++ qml-mode.el
@@ -1,21 +1,42 @@
 ;;; qml-mode.el --- Major mode for editing QT Declarative (QML) code.
-
-;; Copyright (C) 2012 Yen-Chin Lee
-
-;; Author: Yen-Chin Lee <coldnew.tw@gmail.com>
+;;
+;; Copyright (C) 2013 Gregor Klinke
+;;
+;; Author: Gregor Klinke <gck@eyestep.org>
 ;; URL: https://github.com/coldnew/qml-mode
+;;
+;; Original author:
+;; Author: William Xu <william.xwl@gmail.com>
+;; Version: 0.1
+;;
+;; Taken further ideas from qml-mode by Yen-Chin Lee
+;; <coldnew.tw@gmail.com>, original based on a qml-mode by Wen-Chun Lin.
+;; Copyright (C) 2012 Yen-Chin Lee
+;;
 ;; Version: 0.1
 ;; Keywords: qml, qt, qt declarative
-
+;;
 ;; This file is NOT part of GNU Emacs.
-
+;;
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 3, or (at your option) any
+;; later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+;;
 ;;; Commentary:
-
-;;; INTRODUCTION
 ;;
-;; You need this if you are writing QML/Js files with Emacs.
+;; This is a simple major mode for editing Qt QML/JS files.
 ;;
-
 ;;; USAGE
 ;;
 ;; Type `M-x package-install qml-mode`, and add this to your init file:
@@ -24,14 +45,12 @@
 ;;     (add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
 ;;
 
-(require 'generic-x)
-(require 'font-lock)
-(require 'thingatpt)
 
-(defvar qml-mode-indent-offset 4
-  "Indentation offset for `qml-mode'")
+(require 'css-mode)
+(require 'js)
+(require 'cc-mode)
 
-(defvar qml-mode-keywords
+(defvar qml-keywords
   '("Qt" "import" "property"
     "State" "PropertyChanges" "StateGroup" "ParentChange"
     "StateChangeScript" "AnchorChanges" "PropertyAnimation" "NumberAnimation"
@@ -71,13 +90,13 @@
     "undefined"
     ))
 
-(defvar qml-mode-types
+(defvar qml-types
   '("int" "bool" "double" "real"
     "string" "url" "color" "date"
     "variant" "alias" "var"
     "signal" "on" "parent" "as"))
 
-(defvar qml-mode-constants
+(defvar qml-constants
   '("NoButton" "LeftButton" "RightButton" "MidButton"
     "MiddleButton"
     "Horizontal" "Vertical"
@@ -97,106 +116,181 @@
     "StopAtBounds" "DragOverBounds" "DragAndOvershootBounds"
     "VerticalFlick" "HorizontalFlick" "AutoFlickDirection" "HorizontalAndVerticalFlick"))
 
+(defvar qml-builtin-properties
+  '("width" "height" "x" "y" "z"
+    "left" "right"
+    "implicitHeight" "implicitWidth" "preferredWidth" "preferredHeight"
+    "maximumHeight" "minimumHeight"
+    "maximumWidth" "minimumWidth"))
 
-(defun qml-mode:types-list (list)
+
+
+(defvar qml-keywords-pattern
+  (concat "\\<" (regexp-opt qml-keywords) "\\>\\|" js--keyword-re))
+
+(defun qml-types-pattern ()
   ""
   (concat "\\("
-          (mapconcat 'identity list "\\|")
+          (mapconcat 'identity qml-types "\\|")
           "\\|[a-zA-Z0-9_]+\\(<[a-zA-Z0-9_]+>\\)?"
           "\\)"))
 
+(defun qml-builtin-properties-pattern ()
+  ""
+  (concat "[a-zA-Z0-9_]+\\.\\("
+          (mapconcat 'identity qml-builtin-properties "\\|")
+          "\\)"))
+
+(defvar qml-font-lock-keywords
+  `(("/\\*.*\\*/\\|//.*"                ; comment
+     (0 font-lock-comment-face t t))
+    ("\\<\\(true\\|false\\)\\>" ; constants
+     (0 font-lock-constant-face))
+    ,(eval-when-compile
+       (generic-make-keywords-list qml-types 'font-lock-type-face))
+    ,(eval-when-compile
+       (generic-make-keywords-list qml-constants 'font-lock-constant-face))
+    ("\\<id[ \t]*:[ \t]*\\([a-zA-Z0-9_]+\\)" (1 font-lock-constant-face))
+    (,(concat "property[ \t]+" (qml-types-pattern) "+[ \t]+\\([a-zA-Z_]+[a-zA-Z0-9_]*\\)")
+     (1 font-lock-type-face)
+     (3 font-lock-variable-name-face))
+
+    (,(qml-builtin-properties-pattern) (1 font-lock-builtin-face))
+    (,(concat qml-keywords-pattern "\\|\\<parent\\>") ; keywords
+     (0 font-lock-keyword-face nil t))
+    ("\\(function\\|signal\\)\\{1\\}[ \t]+\\([a-zA-Z_]+[a-zA-Z0-9_]*\\)" (2 font-lock-function-name-face))
+    ("\\([a-zA-Z_\\.]+[a-zA-Z0-9_]*\\)[ \t]*:" (1 font-lock-type-face))
+    ("\\([+-]?\\<[0-9]*\\.?[0-9]+[xX]?[0-9a-fA-F]*\\)" (1 font-lock-constant-face))
+;    ("\\<\\([A-Z][a-zA-Z0-9]*\\)\\>"    ; Elements
+;     (1 font-lock-function-name-face nil t)
+;     (2 font-lock-function-name-face nil t))
+    ("\\([a-zA-Z0-9]+\\)[ \t]*{" (1 font-lock-builtin-face))
+    ("\\('[[:alpha:]]*'\\)" (1 font-lock-string-face))
+    )
+  "Keywords to highlight in `qml-mode'.")
+
+
+(defvar qml-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (c-populate-syntax-table table)
+    ;; Comment styles are same as C++
+    (modify-syntax-entry ?/ ". 124b" table)
+    (modify-syntax-entry ?* ". 23" table)
+    (modify-syntax-entry ?\n "> b" table)
+    (modify-syntax-entry ?' "\"" table)
+    table))
 
 ;;;###autoload
-(define-generic-mode qml-mode
-  ;; comments
-  '("//" ("/*" . "*/"))
-  ;; keywords
-  qml-mode-keywords
-  ;; other fontlock
-  (list
-   (eval-when-compile
-     (generic-make-keywords-list qml-mode-types 'font-lock-type-face))
-   (eval-when-compile
-     (generic-make-keywords-list qml-mode-constants 'font-lock-constant-face))
-   (list "\\<id[ \t]*:[ \t]*\\([a-zA-Z0-9_]+\\)" 1 'font-lock-constant-face)
-   (list
-    (concat "property[ \t]+" (qml-mode:types-list qml-mode-types) "+[ \t]+\\([a-zA-Z_]+[a-zA-Z0-9_]*\\)")
-    '(1 font-lock-type-face)
-    '(3 font-lock-variable-name-face))
-   (list "\\(function\\|signal\\)\\{1\\}[ \t]+\\([a-zA-Z_]+[a-zA-Z0-9_]*\\)" 2 'font-lock-function-name-face)
-   (list "\\([a-zA-Z_\\.]+[a-zA-Z0-9_]*\\)[ \t]*:" 1 'font-lock-type-face)
-   (list "\\([+-]?\\<[0-9]*\\.?[0-9]+[xX]?[0-9a-fA-F]*\\)" 1 'font-lock-constant-face)
-   (list "\\([a-zA-Z0-9]+\\)[ \t]*{" 1 'font-lock-builtin-face)
-   (list "\\('[[:alpha:]]*'\\)" 1 'font-lock-string-face)
-   )
-  ;; filetype
-  '("\\.qml$")
+(define-derived-mode qml-mode css-mode "QML"
+  "Major mode for editing Qt QML files.
 
-  ;; initializer
-  (list
-   (function
-    (lambda ()
-      (add-hook 'qml-mode-hook
-                (lambda ()
+Usage:
+------
 
-                  ;; tab width
-                  (set (make-local-variable 'tab-width) qml-mode-indent-offset)
-                  (set (make-local-variable 'indent-tabs-mode) nil)
-                  (set (make-local-variable 'indent-line-function) 'qml-mode-indent-line)
-                  (set (make-local-variable 'indent-region-function) 'qml-mode-indent-region)
-
-                  )
-                nil 'local)
-      ))))
-
-(defun qml-in-comment-p ()
-  "Check whether we are currently in a comment"
-  (let ((here (point)))
-    (and (search-backward "/*" nil t)
-         (prog1
-             (not (search-forward "*/" here t))
-           (goto-char here) ))))
+- WORD/COMMAND COMPLETION:  Typing `\\[qml-expand-abbrev]' after a (not completed) word looks
+  for a word in the buffer or a QML/JS keyword that starts alike, inserts it
+  and adjusts case.  Re-typing `\\[qml-expand-abbrev]' toggles through alternative word
+  completions.
 
 
-(defun qml-mode-indent-line ()
-  "Indent the current line"
-  (if (or (qml-in-comment-p)
-          (looking-at "[ \t]*/\\*") )
-      nil
-    (save-excursion
-      (let ((here (point))
-            (depth 0))
-        (while (and (forward-line -1)
-                    (or (looking-at "^[ \t]*$")
-                        (qml-in-comment-p) ))
-          ;; Jump to a non comment/white-space line
-          )
-        (cond ((looking-at "\\([ \t]*\\)\\([^ \t].*\\)?{[ \t]*$")
-               (setq depth (+ (- (match-end 1) (match-beginning 1))
-                              qml-mode-indent-offset )))
-              ((looking-at "\\([ \t]*\\)[^ \t]")
-               (setq depth (- (match-end 1) (match-beginning 1))) )
-              (t (setq depth 0)) )
-        (goto-char here)
-        (beginning-of-line)
-        (if (looking-at "[ \t]*}")
-            (setq depth (max (- depth qml-mode-indent-offset) 0)) )
-        (if (looking-at "\\([ \t]*\\)")
-            (if (= depth (- (match-end 1) (match-beginning 1)))
-                nil
-              (delete-region (match-beginning 1) (match-end 1))
-              (indent-to depth))
-          (if (> depth 0)
-              (indent-to depth)))))
-    (if (looking-at "^[ \t]*")
-        (forward-whitespace 1))
-    ))
+Key bindings:
+-------------
+
+\\{qml-mode-map}"
+
+  :syntax-table qml-mode-syntax-table
+  (setq font-lock-defaults '(qml-font-lock-keywords))
+  (set (make-local-variable 'comment-start-skip) "\\(//[!]?\\) *")
+  (set (make-local-variable 'comment-start) "//")
+  (set (make-local-variable 'comment-end) "")
+  (set (make-local-variable 'indent-tabs-mode) nil)
+  (set (make-local-variable 'require-final-newline) t)
+  (set (make-local-variable 'comment-column) 40)
+  (set (make-local-variable 'end-comment-column) 79)
+  (run-hooks 'qml-mode-hook))
+
+(define-key qml-mode-map "\M-\C-a" 'qml-beginning-of-defun)
+(define-key qml-mode-map "\M-\C-e" 'qml-end-of-defun)
+(define-key qml-mode-map "\M-\C-h" 'qml-mark-defun)
+(define-key qml-mode-map "\C-c." 'qml-expand-abbrev)
+
+(defconst qml-defun-start-regexp "\{")
+
+(defconst qml-defun-end-regexp "\}")
+
+(defun qml-beginning-of-defun ()
+  "Set the pointer at the beginning of the element within which the pointer is located."
+  (interactive)
+  (re-search-backward qml-defun-start-regexp))
+
+(defun qml-end-of-defun ()
+  "Set the pointer at the beginning of the element within which the pointer is located."
+  (interactive)
+  (re-search-forward qml-defun-end-regexp))
+
+(defun qml-mark-defun ()
+  "Set the region pointer around element within which the pointer is located."
+  (interactive)
+  (beginning-of-line)
+  (qml-end-of-defun)
+  (set-mark (point))
+  (qml-beginning-of-defun))
 
 
-(defun qml-mode-indent-region (start end)
-  (let ((indent-region-function nil))
-    (indent-region start end nil)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Hippie expand customization (for expansion of qml commands)
 
+(defvar qml-abbrev-list
+  (append
+   (list nil) qml-keywords
+	  (list nil) qml-types
+	  (list nil) qml-builtin-properties
+	  (list nil) qml-constants)
+  "Predefined abbreviations for Qml.")
+
+(defvar qml-expand-upper-case nil)
+
+(eval-when-compile (require 'hippie-exp))
+
+(defun qml-try-expand-abbrev (old)
+  "Try expanding abbreviations from `qml-abbrev-list'."
+  (unless old
+    (he-init-string (he-dabbrev-beg) (point))
+    (setq he-expand-list
+          (let ((abbrev-list qml-abbrev-list)
+                (sel-abbrev-list '()))
+            (while abbrev-list
+              (when (or (not (stringp (car abbrev-list)))
+                        (string-match
+                         (concat "^" he-search-string) (car abbrev-list)))
+                (setq sel-abbrev-list
+                      (cons (car abbrev-list) sel-abbrev-list)))
+              (setq abbrev-list (cdr abbrev-list)))
+            (nreverse sel-abbrev-list))))
+  (while (and he-expand-list
+              (or (not (stringp (car he-expand-list)))
+                  (he-string-member (car he-expand-list) he-tried-table t)))
+                                        ;		  (equal (car he-expand-list) he-search-string)))
+    (unless (stringp (car he-expand-list))
+      (setq qml-expand-upper-case (car he-expand-list)))
+    (setq he-expand-list (cdr he-expand-list)))
+  (if (null he-expand-list)
+      (progn (when old (he-reset-string))
+             nil)
+    (he-substitute-string
+     (if qml-expand-upper-case
+         (upcase (car he-expand-list))
+       (car he-expand-list))
+     t)
+    (setq he-expand-list (cdr he-expand-list))
+    t))
+
+
+(defun qml-expand-abbrev (arg))
+(fset 'qml-expand-abbrev (make-hippie-expand-function
+                          '(try-expand-dabbrev
+                            try-expand-dabbrev-all-buffers
+                            qml-try-expand-abbrev)))
 
 (provide 'qml-mode)
 
